0d57ab9a71248935fd687113c7544127ba3a0b97
mimmer <immer@arch.ethz.ch>
2013-05-24 13:51:32 +0200

    added setup and init files

---
+++ __init__.py
@@ -0,0 +1,4 @@
+""" Classes
+
+Classes that implement stuff
+"""
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+import glob, os
+
+#scriptlist = glob.glob(os.path.join('bin', '*.py'))
+
+
+
+setup(name='pyFlowStat',
+      version="1.0",
+      packages=['pyFlowStat'],
+      description='Python tools for statistical analyses of flow data',
+      url='http://www.carmeliet.arch.ethz.ch/',
+      author='ETH/EMPA',
+      author_email='',
+      #scripts=scriptlist,
+      )
+
+try:
+    import numpy
+except ImportError, e:
+        print "\n\n"
+        print "numpy python-package not installed. Please install numpy"
+        print "\n\n"
+
