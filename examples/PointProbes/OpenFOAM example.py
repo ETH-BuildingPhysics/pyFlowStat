a0b73af56b0d6a3e045826b8cad1b435b99e4b07
mimmer <immer@arch.ethz.ch>
2013-11-18 16:25:47 +0100

    Point Probe Example

---
+++ examples/PointProbes/OpenFOAM example.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from pyFlowStat.PointProbe import PointProbe
+
+path=r'Data/10/U'
+pt = PointProbe()
+pt.readFromOpenFoam([0.3,-0.3,0.05],path)
+
+plt.figure()
+ax=plt.plot(pt.data['t'],pt.data['U'])
+plt.xlabel('Time $[s]$')
+plt.ylabel('Velocity $[m/s]$')
+plt.legend(['$U_x$','$U_y$','$U_z$'])
+plt.show()
