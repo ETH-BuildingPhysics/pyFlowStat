c599b4466cf32e10053f689dd802fcb989581fdd
mvonlanthen <marcelv@stutent.ethz.ch>
2013-11-20 12:04:27 +0100

    add example OpenFOAM_statistics.py. Derived from OpenFOAM_example with some extra stuff for statistics.

--- examples/PointProbes/OpenFOAM_example.py
+++ examples/PointProbes/OpenFOAM_example.py
@@ -9,6 +9,8 @@ pt.readFromOpenFoam([0.3,-0.3,0.05],path)
 
 plt.figure()
 ax=plt.plot(pt.data['t'],pt.data['U'])
+plt.title('Time resolved velocity')
+plt.grid(True)
 plt.xlabel('Time $[s]$')
 plt.ylabel('Velocity $[m/s]$')
 plt.legend(['$U_x$','$U_y$','$U_z$'])
