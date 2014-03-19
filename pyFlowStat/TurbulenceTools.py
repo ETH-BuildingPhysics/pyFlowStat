79dfc76c009f18a1b64595ce88ce8d9248627043
mimmer <immer@arch.ethz.ch>
2014-03-19 14:52:03 +0100

    added running average (soothing) to TurbulenceTools

--- pyFlowStat/TurbulenceTools.py
+++ pyFlowStat/TurbulenceTools.py
@@ -92,7 +92,22 @@ def movingave(x, window_len):
     w = np.ones(int(window_len))/float(window_len)
     return np.convolve(x,w,'same')
 
-
+def smooth(x,window_len=5,window='hanning'):
+        if x.ndim != 1:
+                raise ValueError, "smooth only accepts 1 dimension arrays."
+        if x.size < window_len:
+                raise ValueError, "Input vector needs to be bigger than window size."
+        if window_len<3:
+                return x
+        if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+                raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+        s=np.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
+        if window == 'flat': #moving average
+                w=np.ones(window_len,'d')
+        else:  
+                w=eval('np.'+window+'(window_len)')
+        y=np.convolve(w/w.sum(),s,mode='same')
+        return y[window_len:-window_len+1]
 #def movingavesmart(x,window_len,window='flat'):
 #    '''
 #    smooth the data using a window with requested size.
