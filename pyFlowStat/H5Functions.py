3b4bcfa06026a360dfabbc55a1dd4fd8573c23be
mimmer <immer@arch.ethz.ch>
2015-04-14 12:45:33 +0200

    added H5Functions.py. Functions to save and load a dict to/from an h5 file

---
+++ pyFlowStat/H5Functions.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 16:28:00 2015
+
+@author: mimmer
+"""
+
+import h5py
+
+def saveDict(filename,dictionary,keyList=[]):
+    '''
+    Save dict as H5
+    '''
+    if len(keyList)==0:
+        keyList = dictionary.keys()
+    keys = dictionary.keys()
+    fwm = h5py.File(filename, 'w')
+    gDict = fwm.create_group('dict')
+    for k in [k for k in keys if k in keyList]:
+        gDict.create_dataset(k,data=dictionary[k])
+    fwm.close()
+    
+def loadDict(filename,keyList=[]):
+    '''
+    Load dict as H5
+    '''
+    dictionary=dict()
+    fwm = h5py.File(filename, 'r')
+    
+    keys = fwm['dict'].keys()
+    
+    if len(keyList)==0:
+        keyList=keys
+    
+    for k in [k for k in keys if k in keyList]:
+        dictionary[k]=fwm['dict'][k].value
+        
+    fwm.close()
+    return dictionary
