1c53d3e7a6a945d4ac50fa290a7d3afa3600baa6
mimmer <immer@arch.ethz.ch>
2015-04-30 14:31:23 +0200

    added mode parameter to saveDict in H5Functions. Bugfix: Only creates new group if not existing.

--- pyFlowStat/H5Functions.py
+++ pyFlowStat/H5Functions.py
@@ -7,15 +7,19 @@ Created on Fri Mar 13 16:28:00 2015
 
 import h5py
 
-def saveDict(filename,dictionary,keyList=[]):
+def saveDict(filename,dictionary,keyList=[],mode='w'):
     '''
     Save dict as H5
+    mode: 'w' for overwrite, 'w-' for new, 'a' for append
     '''
     if len(keyList)==0:
         keyList = dictionary.keys()
     keys = dictionary.keys()
-    fwm = h5py.File(filename, 'w')
-    gDict = fwm.create_group('dict')
+    fwm = h5py.File(filename, mode)
+    if 'dict' in fwm:
+        gDict=fwm['dict']
+    else:
+        gDict = fwm.create_group('dict')
     for k in [k for k in keys if k in keyList]:
         gDict.create_dataset(k,data=dictionary[k])
     fwm.close()
