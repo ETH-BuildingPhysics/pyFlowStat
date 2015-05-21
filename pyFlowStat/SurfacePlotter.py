f71c5c88782e7ec87c1a0662550e511b906e6eeb
mimmer <immer@arch.ethz.ch>
2015-05-21 13:47:10 +0200

    use new function getMeshgrid in plotting functions

--- pyFlowStat/SurfacePlotter.py
+++ pyFlowStat/SurfacePlotter.py
@@ -16,16 +16,8 @@ def PlotField(ax,surface,field,vmin,vmax,offset=[0,0],interpolation='nearest',mo
     return im
     
 def PlotContour(ax,surface,field,vmin,vmax,offset=[0,0], contourlevels=21, contourlabels=11,**kwargs):
-    ysteps=int((surface.maxY-surface.minY)/surface.dy)+1
-    xsteps=int((surface.maxX-surface.minX)/surface.dy)+1
-    yrange=np.linspace(surface.minY,surface.maxY,ysteps)
-    yrange=np.flipud(yrange)
-    xrange = np.linspace(surface.minX,surface.maxX,xsteps)
 
-    xrange=xrange-offset[0]
-    yrange=yrange-offset[1]
-
-    X,Y = np.meshgrid(xrange, yrange)
+    X,Y = surface.getMeshgrid(offset=offset)
     contour_levels = np.linspace(vmin, vmax, contourlevels)
     contour_levels_label = np.linspace(vmin, vmax, contourlabels)
     print field
@@ -44,15 +36,7 @@ def PlotContour(ax,surface,field,vmin,vmax,offset=[0,0], contourlevels=21, conto
 #    xlim([-30,130])
 #    ylim([-105,130])
 def PlotStreamLine(ax,surface,vmin,vmax,density=10,offset=[0,0],**kwargs):
-    ysteps=int((surface.maxY-surface.minY)/surface.dy)+1
-    xsteps=int((surface.maxX-surface.minX)/surface.dy)+1
-    yrange=np.linspace(surface.minY,surface.maxY,ysteps)
-    yrange=np.flipud(yrange)
-    xrange = np.linspace(surface.minX,surface.maxX,xsteps)
-    
-    xrange=xrange-offset[0]
-    yrange=yrange-offset[1]
-    X,Y = np.meshgrid(xrange, yrange)
+    X,Y = surface.getMeshgrid(offset=offset)
 
     cnorm=mpl.colors.Normalize(vmin=vmin,vmax=vmax)
 
@@ -60,15 +44,7 @@ def PlotStreamLine(ax,surface,vmin,vmax,density=10,offset=[0,0],**kwargs):
 #    return ax.streamplot(X,Y,surface.data['Ux'],surface.data['Uy'],density=density,norm=cnorm,color=u)
 
 def PlotColoredStreamLine(ax,surface,vmin,vmax,density=10,offset=[0,0]):
-    ysteps=int((surface.maxY-surface.minY)/surface.dy)+1
-    xsteps=int((surface.maxX-surface.minX)/surface.dy)+1
-    yrange=np.linspace(surface.minY,surface.maxY,ysteps)
-    yrange=np.flipud(yrange)
-    xrange = np.linspace(surface.minX,surface.maxX,xsteps)
-    
-    xrange=xrange-offset[0]
-    yrange=yrange-offset[1]
-    X,Y = np.meshgrid(xrange, yrange)
+    X,Y = surface.getMeshgrid(offset=offset)
     
     u=np.nan_to_num(np.sqrt(surface.data['Ux']**2+surface.data['Uy']**2))
 
@@ -79,15 +55,9 @@ def PlotColoredStreamLine(ax,surface,vmin,vmax,density=10,offset=[0,0]):
 #    return ax.streamplot(X,Y,surface.data['Ux'],surface.data['Uy'],density=density,norm=cnorm,color=u)
 
 def PlotVelocityVectors(ax,surface,scale=1,offset=[0,0],spacing=1,**kwargs):
-    ysteps=int((surface.maxY-surface.minY)/surface.dy)+1
-    xsteps=int((surface.maxX-surface.minX)/surface.dy)+1
-    yrange=np.linspace(surface.minY,surface.maxY,ysteps)
-    yrange=np.flipud(yrange)
-    xrange = np.linspace(surface.minX,surface.maxX,xsteps)
+ 
     if 'width' not in kwargs:
         kwargs['width']=0.1
         
-    xrange=xrange-offset[0]
-    yrange=yrange-offset[1]
-    X,Y = np.meshgrid(xrange, yrange)
+    X,Y = surface.getMeshgrid(offset=offset)
     return plt.quiver(X[::spacing,::spacing],Y[::spacing,::spacing],surface.data['Ux'][::spacing,::spacing],surface.data['Uy'][::spacing,::spacing],scale=scale,angles='uv',units='xy',**kwargs)
