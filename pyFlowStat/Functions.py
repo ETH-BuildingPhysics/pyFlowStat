0e6b947f84d0006a33ebab5a9152788c3e0567c8
mimmer <immer@arch.ethz.ch>
2015-07-15 08:44:06 +0200

    update to sortNumStrList

--- pyFlowStat/Functions.py
+++ pyFlowStat/Functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sortNumStrList(numStrList):
+def sortNumStrList(numStrList,minVal=None,maxVal=None,step=None):
     '''
     Sort a list of number stored as a list of string. StrNumList is the list of
     number as string.
@@ -15,7 +15,21 @@ def sortNumStrList(numStrList):
             numFltList.append(float(nb))
     numFltList = np.array(numFltList)
     numStrList_idx = np.argsort(numFltList)
-    numStrList_sort = [numStrList[idx] for idx in numStrList_idx]
+    #numFltList_sorted = np.sort(numFltList)
+    
+    if minVal and maxVal:
+        numStrList_idx_filt=[]
+        for idx in numStrList_idx:
+            f=numFltList[idx]
+            if f>=minVal and f<=maxVal:
+                numStrList_idx_filt.append(idx)
+    else:
+        numStrList_idx_filt=numStrList_idx
+    
+    if not step:
+        step=1.0
+    
+    numStrList_sort = [numStrList[idx] for idx in numStrList_idx_filt][::step]
     return numStrList_sort
 
 
