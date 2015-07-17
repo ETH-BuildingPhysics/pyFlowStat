added scaling functions to Surface plus bugfix for floating point problems

pyFlowStat/Surface.py
@@ -634,6 +634,26 @@ class Surface(object):
         zi=zi_ma.filled(np.nan)
 
         return zi
+        
+    def scaleCoordinates(self,factor):
+        self.dx=self.dx*factor
+        self.dy=self.dy*factor
+        self.extent=np.array(self.extent)*factor
+        self.minX=self.minX*factor
+        self.maxY=self.maxY*factor
+        self.maxX=self.maxX*factor
+        self.minY=self.minY*factor
+        
+    def offsetCoordinates(self,offset=[0,0]):
+        self.extent[0]=self.extent[0]-offset[0]
+        self.extent[1]=self.extent[1]-offset[0]
+        self.extent[2]=self.extent[2]-offset[1]
+        self.extent[3]=self.extent[3]-offset[1]
+        self.minX=self.minX-offset[0]
+        self.maxY=self.maxY-offset[1]
+        self.maxX=self.maxX-offset[0]
+        self.minY=self.minY-offset[1]
+        
     def setExtentFromBounds(self):
         '''
         sets self.extent using cell centers minX,maxX,minY,maxY and self.dx/dy
@@ -653,8 +673,8 @@ class Surface(object):
         '''
         returns X and Y meshgrid, usable for contour plotting etc.
         '''
-        ysteps=int((self.maxY-self.minY)/self.dy)+1
-        xsteps=int((self.maxX-self.minX)/self.dy)+1
+        ysteps=int(np.round((self.maxY-self.minY)/self.dy))+1
+        xsteps=int(np.round((self.maxX-self.minX)/self.dy))+1
         yrange=np.linspace(self.minY,self.maxY,ysteps)
         yrange=np.flipud(yrange)
         xrange = np.linspace(self.minX,self.maxX,xsteps)

pyFlowStat/SurfacePlotter.py
@@ -15,26 +15,21 @@ def PlotField(ax,surface,field,vmin,vmax,offset=[0,0],interpolation='nearest',mo
         im=ax.imshow(modifier(surface.data[field]),vmin=vmin,vmax=vmax,interpolation=interpolation,extent=extent,**kwargs)
     return im
     
-def PlotContour(ax,surface,field,vmin,vmax,offset=[0,0], contourlevels=21, contourlabels=11,**kwargs):
+def PlotContour(ax,surface,field,vmin,vmax,offset=[0,0], contourlevels=21, contourlabels=11,alpha=.75,**kwargs):
 
     X,Y = surface.getMeshgrid(offset=offset)
     contour_levels = np.linspace(vmin, vmax, contourlevels)
-    contour_levels_label = np.linspace(vmin, vmax, contourlabels)
+    if contourlabels==0:
+        pass
+    else:
+        contour_levels_label = np.linspace(vmin, vmax, contourlabels)
+        C = ax.contour(X,Y,surface.data[field], contour_levels_label, colors='black', linewidth=.5)
+        ax.clabel(C, inline=1, fontsize=10)
     print field
-    cts=ax.contourf(X,Y,surface.data[field],contour_levels,alpha=.75,**kwargs)
-#    ax.colorbar()
-
-    C = ax.contour(X,Y,surface.data[field], contour_levels_label, colors='black', linewidth=.5)
-    ax.clabel(C, inline=1, fontsize=10)
+    cts=ax.contourf(X,Y,surface.data[field],contour_levels,alpha=alpha,**kwargs)
     ax.set_aspect('equal')
     return cts
 
-#    xfill=[-100,0,0,100,100,200,200,-100]
-#    yfill=[0,0,-100,-100,0,0,-120,-120]
-#    fill(xfill,yfill,'k')
-#    
-#    xlim([-30,130])
-#    ylim([-105,130])
 def PlotStreamLine(ax,surface,vmin,vmax,density=10,offset=[0,0],**kwargs):
     X,Y = surface.getMeshgrid(offset=offset)
 